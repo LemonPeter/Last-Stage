a451234132fa1e77651585b9b1f90deecf18ce22
LemonPeter <1024237508@qq.com>
2018-12-17 21:55:52 +0800

    "this is last-stage"

--- last-stage/src/main/java/com/baizhi/LastStageApplication.java
+++ last-stage/src/main/java/com/baizhi/LastStageApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LastStageApplication {
 
     public static void main(String[] args) {
+        System.out.println("sss");
         SpringApplication.run(LastStageApplication.class, args);
     }
 
