0f72f39333b2ed6262240e5237485df6e3e1ee84
LemonPeter <45873956+LemonPeter@users.noreply.github.com>
2018-12-17 22:14:40 +0800

    Update LastStageApplicationTests.java

--- last-stage/src/test/java/com/baizhi/LastStageApplicationTests.java
+++ last-stage/src/test/java/com/baizhi/LastStageApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LastStageApplicationTests {
-
+System.out.print("hahahahah");
     @Test
     public void contextLoads() {
     }
